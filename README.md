0662d3a7549d3f0c729f1692eb86d963349c0ca5
Marc <marc.stoecker@mindrevolution.com>
2013-11-19 23:00:15 +0100

    Initial version

---
+++ README.md
@@ -0,0 +1,33 @@
+<div style="text-align:right;">
+![mindrevolution](res/logo_go-for-mindrevolution.png)
+</div>
+<p style="font-size:4em;margin-top:100px;border-bottom:4px solid silver;">Contour Useragent Fieldtype</p>
+
+**With this simple Contour field type you can add the current request's useragent string to the form results. If you need to know what browser was used when sending i.e. a feedback or problem report form.**  
+
+##How does it look?
+
+It's a basic Contour field type that you can add to your form easily:
+
+![Screenshot](readme.md.res/Screenshot.png)
+
+## How to install and use?
+Drop the compiled assembly into /bin and also copy over
+
+- /umbraco/Plugins/umbracoContour/Views/FieldType.UseragentString.cshtml
+- /umbraco/Plugins/umbracoContour/images/fieldtypes/useragentstring.png
+
+<!--
+##Download: Ready to use as an Umbraco Package
+You can [download](http://our.umbraco.org/ "Download the Package") "Contour Useragent Fieldtype" as a ready to use Umbraco package and plug it into your site easily.
+-->
+
+##Licence
+Licensed under the [Apache License, Version 2.0](http://www.apache.org/licenses/LICENSE-2.0.html), which allows open source as well as commercial use.
+
+##Author
+Follow [esn303](https://twitter.com/esn303 "@esn303") on Twitter. Like [mindrevolution](https://www.facebook.com/mindrevolution) on Facebook. 
+
+This project is open for collaboration. **Fork. Push. Innovate.**
+
+
---
+++ UseragentField.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Umbraco.Forms.Data;
+using System.Web;
+using Umbraco.Forms.Core;
+
+namespace ContourUseragentField
+{
+    public class UseragentField : Umbraco.Forms.Core.FieldType
+    {
+        public Umbraco.Forms.Core.Controls.HiddenField hf;
+        public List<Object> _value;
+
+         public UseragentField() {
+            //Provider
+            this.Id = new Guid("b0fb4718-6a38-4d8e-8932-e90baf1abb21");
+            this.Name = "Useragent string";
+            this.Description = "Retrieves the useragent string from the visitor's browser";
+            
+             //FieldType
+            this.Icon = "useragentstring.png";
+            this.DataType = Umbraco.Forms.Core.FieldDataType.String;
+           
+            _value = new List<object>();
+        }
+
+
+         public override System.Web.UI.WebControls.WebControl Editor
+         {
+             get
+             {
+                hf = new Umbraco.Forms.Core.Controls.HiddenField();
+                hf.Value = System.Web.HttpContext.Current.Request.UserAgent;
+                return hf;
+             }
+             set
+             {
+                 base.Editor = value;
+             }
+         }
+
+         public override List<Object> Values
+         {
+             get
+             {
+                 if (hf.Value != "")
+                 {
+                     _value.Clear();
+                     _value.Add(hf.Value);
+                 }
+                 return _value;
+             }
+             set
+             {
+                 _value = value;
+             }
+         }
+
+         public override string RenderPreview()
+         {
+             return "<input type=\"hidden\" />";
+         }
+
+         public override string RenderPreviewWithPrevalues(List<object> prevalues)
+         {
+             return RenderPreview();
+         }
+
+         public override bool HideLabel
+         {
+             get
+             {
+                 return true;
+             }
+
+         }
+
+    }
+}
+
+
